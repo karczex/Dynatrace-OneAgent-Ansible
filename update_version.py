94d7e7bf6d913e96ad81f7f3fde61becdec7448b
Paweł Karczewski <pawel.karczewski@dynatrace.com>
2025-05-07 13:05:44 +0200

    Implement version update

--- update_version.py
+++ update_version.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 import argparse
+import yaml
+
+def update_version_in_yaml(file_path, version_field_name , version):
+    with open(file_path, 'r') as file:
+        yaml_data = yaml.safe_load(file)
+    yaml_data[version_field_name] = version
+    print(yaml_data[version_field_name])
+
+    with open(file_path, 'w') as file:
+        yaml.safe_dump(yaml_data, file)
+
+def update_galaxy_yaml(version):
+    update_version_in_yaml("galaxy.yml", "version" ,version)
+
+def update_main_yaml(version):
+    update_version_in_yaml("roles/oneagent/vars/main.yml", "oneagent_script_version",  version)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -10,4 +26,6 @@ if __name__ == "__main__":
     parser.add_argument('new_version')
     args = parser.parse_args()
     print(args.new_version)
+    update_galaxy_yaml(args.new_version)
+    update_main_yaml(args.new_version)
 
